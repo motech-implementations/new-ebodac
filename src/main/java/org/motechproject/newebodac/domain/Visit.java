fea04e37a4b3f7cfaba6d10cd68c29d7a8c4a1c7
Pawel Muchowski <pmuchowski@soldevelo.com>
2019-07-19 18:11:14 +0200

    NEWEBODAC-3: Add FieldConfig and some other entities

--- src/main/java/org/motechproject/newebodac/domain/Visit.java
+++ src/main/java/org/motechproject/newebodac/domain/Visit.java
@@ -1,12 +1,14 @@
 package org.motechproject.newebodac.domain;
 
 import java.time.LocalDate;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
@@ -39,4 +41,11 @@ public class Visit extends BaseEntity {
   @Column(name = "status", nullable = false)
   @Enumerated(EnumType.STRING)
   private EnrollmentStatus status = EnrollmentStatus.NOT_ENROLLED;
+
+  @ManyToOne
+  @JoinColumn(name = "site_id")
+  private Site site;
+
+  @OneToMany(mappedBy = "visit")
+  private Set<ExtraField> extraFields;
 }
