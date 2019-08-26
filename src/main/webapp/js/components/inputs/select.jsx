b2b51541f08bd82813aea668f9408e0d8b180d22
Pawel Muchowski <pmuchowski@soldevelo.com>
2019-08-26 19:03:58 +0200

    NEWEBODAC-3: Parse Select value to simplify component usage

--- src/main/webapp/js/components/inputs/select.jsx
+++ src/main/webapp/js/components/inputs/select.jsx
@@ -1,10 +1,25 @@
+import _ from 'lodash';
 import React from 'react';
 import ReactSelect from 'react-select';
 import PropTypes from 'prop-types';
 
-const Select = ({ onChange, options, ...props }) => {
-  const handleChange = (value) => {
-    onChange(value);
+const Select = ({
+  onChange, options, value, ...props
+}) => {
+  const handleChange = (val) => {
+    if (val) {
+      onChange(val.value);
+    } else {
+      onChange(val);
+    }
+  };
+
+  const getValue = () => {
+    if (!value) {
+      return null;
+    }
+
+    return _.find(options, option => _.isEqual(option, value));
   };
 
   return (
@@ -12,6 +27,7 @@ const Select = ({ onChange, options, ...props }) => {
       {...props}
       options={options}
       onChange={handleChange}
+      value={getValue()}
     />
   );
 };
@@ -22,4 +38,9 @@ Select.propTypes = {
   options: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string,
     PropTypes.shape({})])).isRequired,
   onChange: PropTypes.func.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})]),
+};
+
+Select.defaultProps = {
+  value: null,
 };
