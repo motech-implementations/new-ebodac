motech-implementations/new-ebodac

commit 2ac4613c30e606fd80052c307ad2eb48fbdae079

NEWEBODAC-17 Add csv config

src/main/java/org/motechproject/newebodac/domain/CsvField.java
@@ -1,0 +1,45 @@
+package org.motechproject.newebodac.domain;
+
+import java.util.Map;
+import java.util.UUID;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "csv_field")
+public class CsvField extends BaseEntity {
+
+  @NotBlank
+  @Column(name = "field_name", nullable = false)
+  private String fieldName;
+
+  @NotNull
+  @ManyToOne
+  @JoinColumn(name = "field_config_id", nullable = false)
+  private FieldConfig fieldConfig;
+
+  @Column(name = "format")
+  private String format;
+
+  @Column(name = "default_value")
+  private String defaultValue;
+
+  @ElementCollection(fetch = FetchType.LAZY)
+  @CollectionTable(name = "csv_field_value_map", joinColumns = @JoinColumn(name = "csv_field_id"))
+  @Column(name = "mapped_entity")
+  @MapKeyColumn(name = "field_value")
+  private Map<String, UUID> fieldValueMap;
+}

src/main/java/org/motechproject/newebodac/domain/CsvConfig.java
@@ -1,0 +1,34 @@
+package org.motechproject.newebodac.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.motechproject.newebodac.domain.enums.EntityType;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "csv_config")
+public class CsvConfig extends BaseEntity {
+
+  @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+      CascadeType.REMOVE}, orphanRemoval = true)
+  private Set<CsvField> csvFields = new HashSet();
+
+  @NotNull
+  @Column(name = "entity", nullable = false)
+  @Enumerated(EnumType.STRING)
+  private EntityType entity;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+}

src/main/java/org/motechproject/newebodac/domain/FieldConfig.java
@@ -1,5 +1,6 @@
 package org.motechproject.newebodac.domain;
 
+import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,6 +9,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.motechproject.newebodac.domain.enums.EntityType;
 import org.motechproject.newebodac.domain.enums.FieldType;
@@ -15,6 +17,7 @@ import org.motechproject.newebodac.domain.enums.FieldType;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "field_config")
 public class FieldConfig extends BaseEntity {
 
@@ -69,4 +72,8 @@ public class FieldConfig extends BaseEntity {
 
   @Column(name = "format")
   private String format;
+
+  public FieldConfig(UUID id) {
+    super(id);
+  }
 }

src/main/java/org/motechproject/newebodac/dto/CsvConfigDto.java
@@ -1,0 +1,17 @@
+package org.motechproject.newebodac.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CsvConfigDto extends BaseDto {
+
+  private Set<CsvFieldDto> csvFields = new HashSet();
+
+  private String entity;
+
+  private String name;
+}

src/main/java/org/motechproject/newebodac/dto/CsvFieldDto.java
@@ -1,0 +1,22 @@
+package org.motechproject.newebodac.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CsvFieldDto extends BaseDto {
+
+  private String fieldName;
+
+  private UUID fieldConfigId;
+
+  private String format;
+
+  private String defaultValue;
+
+  private Set<CsvFieldValueToEntityDto> fieldValueMap = new HashSet();
+}

src/main/java/org/motechproject/newebodac/dto/CsvFieldValueToEntityDto.java
@@ -1,0 +1,25 @@
+package org.motechproject.newebodac.dto;
+
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CsvFieldValueToEntityDto extends BaseDto {
+
+  private String fieldValue;
+
+  private UUID entityId;
+
+  /**
+   * Construct new csvFieldValueToEntity with given parameters.
+   * @param fieldValue value of the field
+   * @param entityId id of the entity
+   */
+  public CsvFieldValueToEntityDto(String fieldValue, UUID entityId) {
+    super();
+    this.fieldValue = fieldValue;
+    this.entityId = entityId;
+  }
+}

src/main/java/org/motechproject/newebodac/mapper/CsvConfigMapper.java
@@ -1,0 +1,78 @@
+package org.motechproject.newebodac.mapper;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+import org.motechproject.newebodac.domain.CsvConfig;
+import org.motechproject.newebodac.domain.CsvField;
+import org.motechproject.newebodac.domain.FieldConfig;
+import org.motechproject.newebodac.domain.enums.EntityType;
+import org.motechproject.newebodac.dto.CsvConfigDto;
+import org.motechproject.newebodac.dto.CsvFieldDto;
+import org.motechproject.newebodac.dto.CsvFieldValueToEntityDto;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CsvConfigMapper extends EntityMapper<CsvConfigDto, CsvConfig> {
+
+  CsvConfigMapper INSTANCE = Mappers.getMapper(CsvConfigMapper.class);
+
+  @Override
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "updateDate", ignore = true)
+  @Mapping(target = "createDate", ignore = true)
+  CsvConfig fromDto(CsvConfigDto csvConfigDto);
+
+  @Mapping(target = "updateDate", ignore = true)
+  @Mapping(target = "createDate", ignore = true)
+  @Mapping(target = "fieldConfig", source = "fieldConfigId")
+  CsvField fromDto(CsvFieldDto csvFieldDto);
+
+
+  default Map<String, UUID> fromDto(Set<CsvFieldValueToEntityDto> csvFieldValueMap) {
+    return csvFieldValueMap.stream().collect(Collectors
+        .toMap(CsvFieldValueToEntityDto::getFieldValue, CsvFieldValueToEntityDto::getEntityId));
+  }
+
+  @Mapping(target = "fieldConfigId", source = "fieldConfig")
+  CsvFieldDto toDto(CsvField csvField);
+
+  default Set<CsvFieldValueToEntityDto> toDto(Map<String, UUID> csvFieldValueMap) {
+    return csvFieldValueMap.entrySet().stream().map(elem ->
+        new CsvFieldValueToEntityDto(elem.getKey(), elem.getValue())).collect(Collectors.toSet());
+  }
+
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "updateDate", ignore = true)
+  @Mapping(target = "createDate", ignore = true)
+  void updateFromDto(CsvConfigDto csvConfigDto, @MappingTarget CsvConfig csvConfig);
+
+  /**
+   * Creates String from EntityType.
+   * @param entityType EntityType.
+   * @return String representation of EntityType;
+   */
+  default String fromEntityType(EntityType entityType) {
+    if (entityType == null) {
+      return null;
+    }
+    return entityType.getName();
+  }
+
+  default EntityType toEntityType(String entityType) {
+    return EntityType.getByName(entityType);
+  }
+
+  default FieldConfig toFieldConfig(UUID id) {
+    return new FieldConfig(id);
+  }
+
+  default UUID toUuid(FieldConfig fieldConfig) {
+    return fieldConfig.getId();
+  }
+}

src/main/java/org/motechproject/newebodac/repository/CsvConfigRepository.java
@@ -1,0 +1,12 @@
+package org.motechproject.newebodac.repository;
+
+import java.util.UUID;
+import org.motechproject.newebodac.domain.CsvConfig;
+import org.motechproject.newebodac.domain.enums.EntityType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface CsvConfigRepository extends JpaRepository<CsvConfig, UUID> {
+
+  CsvConfig findByEntity(@Param("entityType") EntityType entityType);
+}

commit 8152dd5d3026d0566b60a9d1c9a0219a6507279a

Fixing the schema validation error that were introduced in https://github.com/motech-implementations/new-ebodac/commit/2ac4613c30e606fd80052c307ad2eb48fbdae079

src/main/java/org/motechproject/newebodac/domain/CsvField.java
@@ -15,6 +15,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 @Getter
 @Setter
@@ -40,6 +41,7 @@ public class CsvField extends BaseEntity {
   @ElementCollection(fetch = FetchType.LAZY)
   @CollectionTable(name = "csv_field_value_map", joinColumns = @JoinColumn(name = "csv_field_id"))
   @Column(name = "mapped_entity")
+  @Type(type = "uuid-char")
   @MapKeyColumn(name = "field_value")
   private Map<String, UUID> fieldValueMap;
 }