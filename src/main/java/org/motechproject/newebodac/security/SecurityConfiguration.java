49082b12c26bf0a8cf2c2f26abdda141070735e2
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2020-02-03 15:36:49 +0100

    test

--- src/main/java/org/motechproject/newebodac/security/SecurityConfiguration.java
+++ src/main/java/org/motechproject/newebodac/security/SecurityConfiguration.java
@@ -44,6 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             "/login",
             "/site/**",
             "/images/**",
+            "/images/icons/**",
             "/manifest.json",
             "/",
             "/importJson/**",
