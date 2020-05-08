a5a24f2c1bc5421e44d740b4572af7ac7111d70c
Pawel Muchowski <pmuchowski@soldevelo.com>
2020-05-08 14:07:15 +0200

    NEWEBODAC-108: Fix prop types validation error in the checkbox field

--- src/main/webapp/js/utils/form/checkbox-field.jsx
+++ src/main/webapp/js/utils/form/checkbox-field.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import BaseField from './base-field';
 import Checkbox from '../inputs/checkbox';
 
 const CheckboxField = (props) => {
-  const renderInput = attributes => (
-    <Checkbox {...attributes} />
+  const renderInput = ({ value, ...attributes }) => (
+    <Checkbox {...attributes} value={!!value} />
   );
+
+  renderInput.propTypes = {
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  };
+
+  renderInput.defaultProps = {
+    value: false,
+  };
 
   return (
     <BaseField
